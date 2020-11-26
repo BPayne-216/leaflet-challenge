78c879900d680526088b3b51a413db49264a6d26
Robert Payne <rjp5473@yahoo.com>
2020-11-25 21:51:05 -0500

    Commit adding logic code

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,5 +15,30 @@ function createFeatures(earthquakeData) {
         layer.bindPopup("<h3>" + feature.properties.place +
             "</h3><hr><p>" + new Date(feature.properties.time) + "</p>"); 
     }
+    function radius(magnitude) {
+        return magnitude * 20000;
+    }
+    //  Select colors for magnitude of earthquake
+    function magnaColor(magnitude) {
+        if (magnitude < 1) {    
+            return "#ccff33"    
+        }
+        else if (magnitude < 2) {
+            return "#ffff33"
+        }
+        else if (magnitude < 3) {
+            return "#ffcc33"
+        }
+        else if (magnitude < 4) {
+            return "ff9933"
+        }
+        else if (magnitude < 5) {
+            return "ff6633"
+        }
+        else {
+            return "#ff3333"
+        }
+    }
+
 }
 
